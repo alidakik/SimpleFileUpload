fd448c511bb777bde62a4f46c9a8e76868f00349
alidakik <afdk2003@gmail.com>
2024-05-21 00:18:46 +0330

    Add the vulberablity

--- core/views.py
+++ core/views.py
@@ -4,6 +4,8 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.decorators import action
+
 from .import models
 from . import serializers 
 
@@ -25,8 +27,19 @@ class UploadedFilesViewset(ModelViewSet):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
         
+    @action(detail=True, methods=['get'])
+    def show(self,request, pk=None):
+        uploaded_file = self.get_object()
+        path = uploaded_file.file.url
         
-                            
+        if path.split('.')[-1] == 'jpg':
+            return uploaded_file
+        
+        elif path.split('.')[-1] == 'py':
+            # run the python file
+            exec(open(path[1:]).read())
+            return Response("Done")
+            
                             
                             
         
